0c5f415f0c1e885b123a0fb51ed9a6185281a7a0
Samsul Arif Zulvian <saarizu@gmail.com>
2020-10-03 11:03:08 +0700

    tambahkan url download dan homescreen

---
+++ resources/views/homescreen.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TWA</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}?version=2">
+</head>
+<body>
+    <div style="height: 100vh; display: flex; align-items: center; justify-content: center">
+        <div>
+            <div>
+                <img src="/img/logo.png" alt="logo">
+            </div>
+            <div class="px-5 mt-3">
+                <a href="/" class="btn btn-danger btn-block">MULAI BELAJAR</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,15 @@ use App\Http\Controllers\FormController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hs', function () {
+    return view('homescreen');
+});
+
+Route::get('/download', function () {
+    return response()
+        ->download(public_path('files/app-release.apk'));
+});
+
 Route::get('/', HomeController::class);
 
 Route::resource('/forms', FormController::class)->only(['show']);
